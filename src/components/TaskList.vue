7a981a0578e46839a428092f13513d0b6f3e1351
Ahmed <atomcgarry@gmail.com>
2021-12-21 18:53:38 +0000

    conditionally rendered all tasks complete statement

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,12 +1,13 @@
 <template>
   <div>TaskList</div>
   <div>
-    <ul>
+    <ul v-if="this.tasks.length > 0">
       <li v-for="(task, index) of tasks" :key="task.name">
         {{ task.name }}
         <button @click="() => deleteHandler(index)">Delete</button>
       </li>
     </ul>
+    <h1 v-else>All Tasks Complete</h1>
   </div>
 </template>
 
